5c0c52540444c76833197682fc97dcfa0f4ef218
kgilden <karl.gildenhuys@gmail.com>
2024-06-03 16:42:50 +1200

    add language drop down

--- src/Views/index.php
+++ src/Views/index.php
@@ -16,13 +16,23 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Language
+            <?php 
+              switch($iso){
+                case "mi":
+                  echo "Māori";
+                  break;
+                
+                case "tl":
+                  echo "Tagalog";
+                  break;
+                default:
+                  echo "Select a language";
+              }
+            ?>
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="/library/mi">Māori</a></li>
+            <li><a class="dropdown-item" href="/library/tl">Tagalog</a></li>
           </ul>
         </li>
       </ul>
